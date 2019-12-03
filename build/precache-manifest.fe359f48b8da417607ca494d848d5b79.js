9f52d9ae718cf9bcc172a5c74d2c039c7be8798c
Shayne Meyer <shayne.meyer@gmail.com>
2019-12-03 08:54:29 -0800

    updated font

--- build/precache-manifest.fe359f48b8da417607ca494d848d5b79.js
+++ build/precache-manifest.fe359f48b8da417607ca494d848d5b79.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "63c63b430622429aeae8b927d7d449f4",
+    "revision": "f55ccf31c7eab09c479f8f5661e184e3",
     "url": "/lights-out-game/index.html"
   },
   {
-    "revision": "cd8185f677c91df98e8e",
-    "url": "/lights-out-game/static/css/main.d2a3308d.chunk.css"
+    "revision": "fe123e0e7640b48434f0",
+    "url": "/lights-out-game/static/css/main.caa34b1d.chunk.css"
   },
   {
     "revision": "c2a6b9d7d00ee090eb90",
     "url": "/lights-out-game/static/js/2.1fdc582c.chunk.js"
   },
   {
-    "revision": "cd8185f677c91df98e8e",
+    "revision": "fe123e0e7640b48434f0",
     "url": "/lights-out-game/static/js/main.a9994aa7.chunk.js"
   },
   {
